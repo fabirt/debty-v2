d53349ef6a0697437df169bb968c2f611e26d39f
Fabirt <fandig27@gmail.com>
2021-03-21 21:59:46 -0500

    Refactoring

--- android/app/src/main/java/com/fabirt/debty/ui/summary/SummaryFragment.kt
+++ android/app/src/main/java/com/fabirt/debty/ui/summary/SummaryFragment.kt
@@ -9,6 +9,7 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.fabirt.debty.databinding.FragmentSummaryBinding
 import com.fabirt.debty.domain.model.Person
 import com.fabirt.debty.util.toCurrencyString
@@ -39,6 +40,8 @@ class SummaryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.rvPeople.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
         binding.rvPeople.adapter = personSummaryAdapter
         binding.tvBalanceAmount.text = (0).toCurrencyString()
         binding.tvNegativeAmount.text = (0).toCurrencyString()
