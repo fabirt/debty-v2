77d0094576d25b301d3c917a00abbd4b17c0561b
Fabirt <fandig27@gmail.com>
2021-03-22 19:32:48 -0500

    Adding fields to create movement fragment

--- android/app/src/main/java/com/fabirt/debty/ui/movement/create/CreateMovementFragment.kt
+++ android/app/src/main/java/com/fabirt/debty/ui/movement/create/CreateMovementFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.fabirt.debty.R
 import com.fabirt.debty.databinding.FragmentCreateMovementBinding
+import com.fabirt.debty.util.toDateString
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointBackward
+import com.google.android.material.datepicker.MaterialDatePicker
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,8 +27,45 @@ class CreateMovementFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.editTextDate.setOnClickListener {
+            openDatePicker()
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun openDatePicker() {
+        val calendarConstraints =
+            CalendarConstraints.Builder()
+                .setValidator(DateValidatorPointBackward.now())
+                .build()
+
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText(getString(R.string.date_picker_title))
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .setCalendarConstraints(calendarConstraints)
+                .build()
+
+        datePicker.show(childFragmentManager, "mtrl_date_picker")
+
+        datePicker.addOnPositiveButtonClickListener {
+            binding.editTextDate.setText(it.toDateString())
+        }
+        datePicker.addOnNegativeButtonClickListener {
+            // Respond to negative button click.
+        }
+        datePicker.addOnCancelListener {
+            // Respond to cancel button click.
+        }
+        datePicker.addOnDismissListener {
+            // Respond to dismiss events.
+        }
     }
 }
