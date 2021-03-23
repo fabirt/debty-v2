0d711cfe9c5c64f99b633e27e706917471996f85
Fabirt <fandig27@gmail.com>
2021-03-23 06:24:34 -0500

    Fixing shortcut issue

--- android/app/src/main/java/com/fabirt/debty/ui/MainActivity.kt
+++ android/app/src/main/java/com/fabirt/debty/ui/MainActivity.kt
@@ -3,10 +3,12 @@ package com.fabirt.debty.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.fabirt.debty.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -16,21 +18,27 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.Theme_App)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_main)
-        handleIntent()
+
+        // Not recreating the activity.
+        if (savedInstanceState == null) {
+            handleIntent()
+        }
     }
 
     private fun handleIntent() {
         intent?.dataString?.let { data ->
-            when(data) {
+            when (data) {
                 "debty.movement" -> triggerNewMovementDeepLink()
             }
         }
     }
 
     private fun triggerNewMovementDeepLink() {
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        navController.navigate(R.id.personSearchFragment)
+        lifecycleScope.launch {
+            val navHostFragment = supportFragmentManager
+                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            val navController = navHostFragment.navController
+            navController.navigate(R.id.personSearchFragment)
+        }
     }
 }
