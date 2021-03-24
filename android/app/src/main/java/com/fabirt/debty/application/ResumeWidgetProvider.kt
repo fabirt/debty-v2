7887992d5d1bf0e0f5aa28a54ac6a61b3b944f64
Fabirt <fandig27@gmail.com>
2021-03-24 12:44:52 -0500

    Creating pending intent to update app widget on click

--- android/app/src/main/java/com/fabirt/debty/application/ResumeWidgetProvider.kt
+++ android/app/src/main/java/com/fabirt/debty/application/ResumeWidgetProvider.kt
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.widget.RemoteViews
 import com.fabirt.debty.R
 import com.fabirt.debty.data.db.dao.MovementDao
@@ -27,14 +28,24 @@ class ResumeWidgetProvider : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
+        Log.i("ResumeWidgetProvider", "onUpdate")
         // Perform this loop procedure for each App Widget that belongs to this provider
         appWidgetIds.forEach { appWidgetId ->
-            // Create an Intent to launch ExampleActivity
-            val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
-                .let { intent ->
-                    PendingIntent.getActivity(context, 0, intent, 0)
-                }
+            // Create an Intent to launch MainActivity
+            val intentActivity = Intent(context, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            }
+            val pendingIntentActivity =
+                PendingIntent.getActivity(
+                    context,
+                    0,
+                    intentActivity,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
 
+            val pendingIntentUpdate = getUpdateSelfPendingIntent(context, appWidgetIds)
+
+            // Do Work
             GlobalScope.launch {
                 val totalOweMe = dao.getTotalOweMe()?.absoluteValue ?: 0.0
                 val totalIOwe = dao.getTotalIOwe()?.absoluteValue ?: 0.0
@@ -45,14 +56,27 @@ class ResumeWidgetProvider : AppWidgetProvider() {
                     context.packageName,
                     R.layout.widget_resume
                 ).apply {
-                    setOnClickPendingIntent(R.id.container, pendingIntent)
                     setTextViewText(R.id.tv_owe_me_value, totalOweMe.toCurrencyString())
                     setTextViewText(R.id.tv_i_owe_value, totalIOwe.toCurrencyString())
+
+                    setOnClickPendingIntent(R.id.container, pendingIntentActivity)
+                    setOnClickPendingIntent(R.id.btn_refresh_widget, pendingIntentUpdate)
                 }
 
                 // Tell the AppWidgetManager to perform an update on the current app widget
                 appWidgetManager.updateAppWidget(appWidgetId, views)
             }
         }
+    }
+
+    private fun getUpdateSelfPendingIntent(
+        context: Context,
+        appWidgetIds: IntArray
+    ): PendingIntent {
+        val intent = Intent(context, ResumeWidgetProvider::class.java).apply {
+            action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
+            putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds)
+        }
+        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
     }
 }
