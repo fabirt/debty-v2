00d702e6cab0ee490bf7ecbcdf4adc9ac1c94888
Fabi Diartt <fandig27@gmail.com>
2021-05-24 19:03:16 -0500

    Fixing in app updates

--- android/app/src/main/java/com/fabirt/debty/ui/MainActivity.kt
+++ android/app/src/main/java/com/fabirt/debty/ui/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.fabirt.debty.ui
 
-import android.app.PendingIntent
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.WindowInsets
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
@@ -17,8 +13,6 @@ import com.fabirt.debty.util.RootViewDeferringInsetsCallback
 import com.google.android.play.core.appupdate.AppUpdateInfo
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
-import com.google.android.play.core.install.InstallState
-import com.google.android.play.core.install.InstallStateUpdatedListener
 import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.InstallStatus
 import com.google.android.play.core.install.model.UpdateAvailability
@@ -55,15 +49,35 @@ class MainActivity : AppCompatActivity() {
         }
 
         appUpdateManager = AppUpdateManagerFactory.create(this)
+
         // Not recreating the activity.
         if (savedInstanceState == null) {
             handleIntent()
         }
+
+        checkUpdate()
     }
 
     override fun onResume() {
         super.onResume()
-        checkUpdate()
+        appUpdateManager
+            .appUpdateInfo
+            .addOnSuccessListener { updateInfo ->
+                if (updateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+                    appUpdateManager.completeUpdate()
+                }
+
+                if (updateInfo.updateAvailability()
+                    == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS
+                ) {
+                    appUpdateManager.startUpdateFlowForResult(
+                        updateInfo,
+                        AppUpdateType.IMMEDIATE,
+                        this,
+                        APP_UPDATE_REQUEST_CODE
+                    )
+                }
+            }
     }
 
 
@@ -85,117 +99,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkUpdate() {
-        Log.i(TAG, "checkUpdate")
-        val updateInfoTask = appUpdateManager.appUpdateInfo
-
-        updateInfoTask.addOnSuccessListener { updateInfo ->
-            val availability = updateInfo.updateAvailability()
-            if (availability == UpdateAvailability.UPDATE_AVAILABLE) {
-                startUpdateFlow(appUpdateManager, updateInfo)
-            } else if (availability == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
-                startUpdateFlow(appUpdateManager, updateInfo)
+        appUpdateManager
+            .appUpdateInfo
+            .addOnSuccessListener { updateInfo ->
+                val availability = updateInfo.updateAvailability()
+                if (availability == UpdateAvailability.UPDATE_AVAILABLE) {
+                    startUpdateFlow(appUpdateManager, updateInfo)
+                }
+            }.addOnFailureListener { error ->
+                Log.e(TAG, error.toString())
             }
-            if (updateInfo.installStatus() == InstallStatus.DOWNLOADED) {
-                appUpdateManager.completeUpdate()
-            }
-        }.addOnFailureListener { error ->
-            Log.e(TAG, error.toString())
-        }
     }
 
     private fun startUpdateFlow(updateManager: AppUpdateManager, appUpdateInfo: AppUpdateInfo) {
-        updateManager.registerListener(object : InstallStateUpdatedListener {
-            override fun onStateUpdate(state: InstallState) {
-                when (state.installStatus()) {
-                    InstallStatus.DOWNLOADING -> {
-                        showDownloadStartNotification()
-                    }
-                    InstallStatus.DOWNLOADED -> {
-                        showDownloadedNotification()
-                        updateManager.unregisterListener(this)
-                    }
-                    InstallStatus.FAILED -> {
-                        showDownloadErrorNotification()
-                        updateManager.unregisterListener(this)
-                    }
-                    InstallStatus.CANCELED -> {
-                        showDownloadErrorNotification()
-                        updateManager.unregisterListener(this)
-                    }
-                    else -> {
-                    }
-                }
-            }
-        })
-
         updateManager.startUpdateFlowForResult(
             appUpdateInfo,
             AppUpdateType.FLEXIBLE,
             this,
             APP_UPDATE_REQUEST_CODE
         )
-    }
-
-    private fun showDownloadStartNotification() {
-        val intent = Intent(this, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        }
-        val pendingIntent = PendingIntent.getActivity(
-            this, 0,
-            intent, PendingIntent.FLAG_UPDATE_CURRENT
-        )
-
-        val notification = NotificationCompat.Builder(this, K.APP_UPDATE_NOTIFICATION_CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_stat_download)
-            .setColor(getColor(R.color.colorPrimary))
-            .setContentTitle(getString(R.string.app_update_downloading))
-            .setProgress(0, 0, true)
-            .setAutoCancel(false)
-            .setOngoing(true)
-            .setContentIntent(pendingIntent)
-            .build()
-        NotificationManagerCompat.from(this).notify(K.APP_UPDATE_NOTIFICATION_ID, notification)
-    }
-
-    private fun showDownloadedNotification() {
-        val intent = Intent(this, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        }
-        val pendingIntent = PendingIntent.getActivity(
-            this, 0,
-            intent, PendingIntent.FLAG_UPDATE_CURRENT
-        )
-
-        val notification = NotificationCompat.Builder(this, K.APP_UPDATE_NOTIFICATION_CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_stat_download)
-            .setColor(getColor(R.color.colorPrimary))
-            .setContentTitle(getString(R.string.app_update_downloaded))
-            .setProgress(0, 0, false)
-            .setAutoCancel(true)
-            .setOngoing(true)
-            .setContentIntent(pendingIntent)
-            .build()
-        NotificationManagerCompat.from(this).notify(K.APP_UPDATE_NOTIFICATION_ID, notification)
-    }
-
-    private fun showDownloadErrorNotification() {
-        val intent = Intent(this, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        }
-        val pendingIntent = PendingIntent.getActivity(
-            this, 0,
-            intent, PendingIntent.FLAG_UPDATE_CURRENT
-        )
-
-        val notification = NotificationCompat.Builder(this, K.APP_UPDATE_NOTIFICATION_CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_stat_download)
-            .setColor(getColor(R.color.colorPrimary))
-            .setContentTitle(getString(R.string.app_update_canceled))
-            .setProgress(0, 0, false)
-            .setAutoCancel(true)
-            .setOngoing(false)
-            .setContentIntent(pendingIntent)
-            .build()
-        NotificationManagerCompat.from(this).notify(K.APP_UPDATE_NOTIFICATION_ID, notification)
     }
 }
